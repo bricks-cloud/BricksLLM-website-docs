c2f87396278f5007c36871a8089e6bd38748554f
Donovan So <donovanso@yahoo.com.hk>
2024-02-05 19:51:45 -0800

    add github page configs

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -13,16 +13,16 @@ const config = {
   favicon: "img/favicon.ico",
 
   // Set the production url of your site here
-  url: "https://your-docusaurus-site.example.com",
+  url: "https://bricks-cloud.github.io",
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: "/",
+  baseUrl: "/BricksLLM-website-docs/",
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
-  organizationName: "facebook", // Usually your GitHub org/user name.
-  projectName: "docusaurus", // Usually your repo name.
-
+  organizationName: "bricks-cloud", // Usually your GitHub org/user name.
+  projectName: "BricksLLM-website-docs", // Usually your repo name.
+  trailingSlash: false,
   onBrokenLinks: "throw",
   onBrokenMarkdownLinks: "warn",
 
