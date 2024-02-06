c9e7a80d686da12ea68a65a182e4f01ae5692ab6
Donovan So <donovanso@yahoo.com.hk>
2024-02-06 11:17:32 -0800

    update favicon and page title

--- src/pages/index.js
+++ src/pages/index.js
@@ -33,7 +33,7 @@ export default function Home() {
   const { siteConfig } = useDocusaurusContext();
   return (
     <Layout
-      title={`Hello from ${siteConfig.title}`}
+      title="BricksAI Documentation"
       description="Description will go into a meta tag in <head />"
     >
       <HomepageHeader />
